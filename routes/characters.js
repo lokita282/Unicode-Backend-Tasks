dc704ed4f8e555f59f988553be4fbd0b0ecc442b
lokita282 <lokitavarma282@gmail.com>
2021-08-05 00:50:02 +0530

    Task 4 - Bonus

--- routes/characters.js
+++ routes/characters.js
@@ -5,7 +5,7 @@ const Character = require('../models/character')
 
 
 
-//Post request
+//Post request (Task3)
 router.post('/store-to-database', async (req, res) => {
   const character = new Character({
     ...req.body
@@ -20,7 +20,7 @@ router.post('/store-to-database', async (req, res) => {
   }
 })
 
-//Delete request
+//Delete request (Task 4)
 router.delete('/:id', async(req,res) => {
   try{
     const character = await Character.findByIdAndDelete({ _id: req.params.id })
@@ -31,6 +31,26 @@ router.delete('/:id', async(req,res) => {
   } catch(e){
     res.status(500).send()
     console.log(e)
+  }
+})
+
+//Update Request (Task 4 - Bonus)
+router.put('/:id', async (req, res) => {
+  try{
+    const updates = Object.keys(req.body)
+    const character = await Character.findById(req.params.id)
+
+    if(!character){
+      res.status(404).send()
+    }
+
+    updates.forEach((update) => character[update] = req.body[update])
+    await character.save()
+
+    res.send(character)
+
+  } catch(e){
+    res.status(400).send()
   }
 })
 
